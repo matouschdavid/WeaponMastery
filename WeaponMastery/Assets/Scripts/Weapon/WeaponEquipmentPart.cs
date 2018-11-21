378526cf24d8966d148eb4386ceab1415b0d980f
Christoph <nrgchrisi@gmail.com>
2018-11-21 19:52:42 +0100

    added folders

---
+++ WeaponMastery/Assets/Scripts/Weapon/WeaponEquipmentPart.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponEquipmentPart : MonoBehaviour
+{
+    public Sprite WeaponPartSprite;
+    public WeaponPartType WeaponPartType;
+    public WeaponEquipmentUI EquipmentUi;
+    public int Id;
+
+    public Image WeaponPartImage;
+
+    public virtual void OnImpact()
+    {
+
+    }
+
+    public virtual void OnFire()
+    {
+
+    }
+
+    public void OnDrag()
+    {
+        Debug.Log("Is dragging");
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnRelease()
+    {
+
+        Debug.Log("Is releasing");
+        EquipmentUi.ReleasedEquipmentPart(this);
+
+
+    }
+
+    public void OnGrab()
+    {
+
+        Debug.Log("Is grabbing");
+
+    }
+}
+
+public enum WeaponPartType
+{
+    Stat,
+    Bullet,
+    Impact
+}
