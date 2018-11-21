df884439cc2d5e6fc76aadbf131d87ca424ea698
David <matousch.david@gmail.com>
2018-11-21 11:53:35 +0100

    Weaponbuildingsystem kinda finished

---
+++ WeaponMastery/Assets/Scripts/WeaponEquipment.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponEquipment : MonoBehaviour
+{
+    public GameObject EquipmentUI;
+
+    public Weapon Weapon;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void ChangeEquipment()
+    {
+        EquipmentUI.SetActive(true);
+
+    }
+}
