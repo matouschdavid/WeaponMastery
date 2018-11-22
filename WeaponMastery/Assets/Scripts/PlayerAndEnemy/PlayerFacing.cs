cab9e01aac9dbed2f08e3cf9929bd64352babfb1
Christoph <nrgchrisi@gmail.com>
2018-11-22 22:06:44 +0100

    added directional sprite changing

---
+++ WeaponMastery/Assets/Scripts/PlayerAndEnemy/PlayerFacing.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFacing : MonoBehaviour {
+
+    public Sprite[] PlayerSprites;
+    public Transform WeaponPos;
+    private float ratio;
+    private SpriteRenderer spriteRenderer;
+
+    void Start(){
+
+        ratio = 360 / PlayerSprites.Length;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update() {
+
+        if (Input.GetKeyDown(KeyCode.Tab)) {
+
+            Debug.Log(WeaponPos.rotation.z);
+            Debug.Log(WeaponPos.rotation.eulerAngles.z);
+        }
+    }
+
+	void FixedUpdate () {
+
+        for (int i = PlayerSprites.Length - 1; i >= 0; i--) {
+            if (WeaponPos.rotation.eulerAngles.z - ratio / 2 <= ratio * (i + 1)) {
+
+                spriteRenderer.sprite = PlayerSprites[i];
+            }
+        }
+		
+	}
+}
