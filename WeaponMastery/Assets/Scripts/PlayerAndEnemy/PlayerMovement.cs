0cd0995120888e2e4cea710f13eb5e0231591859
Christoph <nrgchrisi@gmail.com>
2018-11-22 20:38:11 +0100

    added player-movement

---
+++ WeaponMastery/Assets/Scripts/PlayerAndEnemy/PlayerMovement.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    #region Movement(x,y)
+
+    public float RunSpeed;
+    private Vector2 moveVelocity;
+    private Vector2 moveInput;
+
+    #endregion
+    #region dash
+
+    public float DashSpeed;
+    public float DashTime;
+    private bool isInDash;
+
+    #endregion
+
+    private Rigidbody2D rb;
+
+    void Start() {
+
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    void Update() {
+
+        #region Movement(x,y)
+
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+
+        #endregion
+
+        #region dash
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            StartCoroutine(Dash());
+        }
+
+        #endregion
+    }
+
+    private IEnumerator Dash() {
+        isInDash = true;
+        rb.velocity = moveInput * DashSpeed;
+        yield return new WaitForSeconds(DashTime);
+        isInDash = false;
+    }
+
+    void FixedUpdate() {
+
+        #region Movement(x,y)
+
+        if (!isInDash) {
+
+            rb.MovePosition(rb.position + moveInput * RunSpeed * Time.fixedDeltaTime);
+        }
+
+        #endregion
+    }
+}
